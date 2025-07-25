0edcd49d7c16ca8a82172082c87c71e7057b9b62
Siddharth Sharabu <siddharthsharabu125@gmail.com>
2025-07-25 08:42:53 +0530

    config edits

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -11,6 +11,22 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['placehold.co', 'i.imgur.com'],
   },
+  
+  // Disable Webpack cache
+  webpack(config, { dev, isServer }) {
+    // Disable caching for builds
+    if (!dev) {
+      config.cache = false;
+    }
+    return config;
+  },
+  
+  // Disable experimental caching on the server side
+  
+  // Optional: Prevent creation of the .next folder completely (for export use case)
+  // If you are exporting a static site and don't need SSR, you could switch to next export
+  // But this part is for export use cases only:
+  // target: 'serverless',
 };
 
 export default nextConfig;
